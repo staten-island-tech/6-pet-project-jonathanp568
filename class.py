57ac24fc954b12f43e57c81422b0f7b35a7f8d7a
jonathanp568 <jonathanpan0510@icloud.com>
2025-10-24 09:49:15 -0400

    s

---
+++ class.py
@@ -0,0 +1,9 @@
+class pet():
+    def __init__(self, name, food):
+        self.food = food
+        self.name = name
+    def feed(self, feed):
+        food += feed
+        return self.food
+x = pet("i", 20)
+print(x.name)
--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-# class pet():
-#     def __init__(self, food):
-#         self.food = food
-#         return self.food
-#     def feed(self, feed):
-#         food += feed
-#         return self.food
-# pet.__init__(10)
-
 def slots(q, x, y, z):
     quarters = q
     slots1 = x
@@ -16,25 +7,25 @@ def slots(q, x, y, z):
     while quarters != 0:
         if slots1 != 35 and quarters != 0:
             slots1 += 1
-            quarters -= 1
             plays += 1
+            quarters -= 1
         elif slots1 == 35:
             quarters += 30
-            slots1 -= 35
+            slots1 -= 34
         if slots2 != 100 and quarters != 0:
             slots2 += 1
-            quarters -= 1
             plays += 1
+            quarters -= 1
         elif slots2 == 100:
             quarters += 60
-            slots2 -= 100
+            slots2 -= 99
         if slots3 != 10 and quarters != 0:
             slots3 += 1
-            quarters -= 1
             plays += 1
+            quarters -= 1
         elif slots3 == 10:
             quarters += 9
-            slots3 -= 10
+            slots3 -= 9
     print(f"Martha plays {plays} times before going broke.")
 
 slots(77, 4, 9, 3)
