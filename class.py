c04c77d91d2161efedc35880dbfd58efba6b47cd
jonathanp568 <jonathanpan0510@icloud.com>
2025-10-27 10:16:47 -0400

    Update class.py

--- class.py
+++ class.py
@@ -1,9 +1,13 @@
 class pet():
-    def __init__(self, name, food):
+    def __init__(self, name, food, water):
         self.food = food
         self.name = name
+        self.water = water
     def feed(self, feed):
-        food += feed
-        return self.food
-x = pet("i", 20)
-print(x.name)
+        self.food += feed
+        print(self.food)
+    def feed(self, drink):
+        self.water += drink
+        print(self.water)
+attributes = pet("i", 10, 10)
+attributes.feed(10)
