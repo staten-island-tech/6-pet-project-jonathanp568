aa7adbb33559f40667ad0dd52b69fa1b51f8a768
jonathanp568 <jonathanpan0510@icloud.com>
2025-11-20 09:40:46 -0500

    Update guard.py

--- guard.py
+++ guard.py
@@ -1,16 +1,14 @@
 def valid(email, password):
-    if not isinstance(email, str):
-        return "invalid email, not a string"
+    if not isinstance(email, str) or not isinstance(password, str):
+        return "invalid email/password, not a string"
     if "@" not in email:
         return "invalid email, no @"
-    if not isinstance(password, str):
-        return "invalid password, not a string"
     if len(password) < 8:
         return "invalid password, not 8 characters long"
-    if password.isdigit() < 1:
+    if not any(ch.isdigit() for ch in password):
         return "invalid password, no number"
-    if password.isupper() < 1:
+    if not any(ch.isupper() for ch in password):
         return "invalid password, no uppercase letters"
-    print(valid.__dict__)
+    print(email, password)
     return "accepted"
-print(valid("ejjsef@", "daddsf22sf"))
+print(valid("ejjsef@", "jsdgiueoAii1"))
